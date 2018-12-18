cc4ffc738214153eefbd3cf62d0cb7eb3dbb3113
David Wilson <w.dave.w@gmail.com>
2018-12-18 01:28:23 -0800

    Finally finish day 15

--- exe/Day15P1.hs
+++ exe/Day15P1.hs
@@ -1,7 +1,32 @@
 import Day15
 import Control.Concurrent
 import Control.Monad
+import qualified Data.Map as M
+import Debug.Trace
+
+runGame :: GameState -> IO ()
+runGame gs = forever $ do
+  putStrLn $(show $ _complete gs)
+  putStrLn $(show $ _rounds gs)
+  putStrLn $ show $ length . M.keys . M.filter ((== Elf) . _faction) $ (_units gs)
+  putStrLn $ printBoard gs
+  threadDelay 1000000
+  runGame (nextRound . resolveRound $ gs)
+
+-- too tired / late to do binary search with this, just going to brute force it while I take a break
+findElfAP :: Int -> GameState -> Int
+findElfAP ap gs
+  | finalElves == initialElves = ap
+  | otherwise = trace (show finalElves ++ " " ++ show initialElves) $ findElfAP (ap + 1) gs
+  where modifiedStart = gs { _units = M.map (\u -> if (_faction u ) == Elf then u { _ap = ap } else u) (_units gs) }
+        finalState = head . dropWhile (not . _complete) $ (iterate (nextRound . resolveRound) modifiedStart)
+        finalElves = length . M.keys . M.filter ((== Elf) . _faction) $ (_units finalState)
+        initialElves = length . M.keys . M.filter ((== Elf) . _faction) $ (_units gs)
 
 main :: IO ()
 main =  do
-  resolveROund
+  t <- readFile "./input/day15.txt"
+  putStrLn $ show $ (\(a, b) -> (a * b)) . summarizeGame . head . dropWhile (not . _complete) $ (iterate (nextRound . resolveRound) (moreAp $ initState t))
+  putStrLn $ show $ findElfAP 14 (initState t)
+  where initState t = (nextTurn (linesToBoard emptyState (lines t)))
+        moreAp state = state { _units = M.map (\u -> if (_faction u ) == Elf then u { _ap = 15 } else u) (_units state) }
--- src/Day15.hs
+++ src/Day15.hs
@@ -17,21 +17,22 @@ data Faction = Elf | Goblin
   deriving (Show, Eq)
 
 data Unit = Unit {
-   _id :: Int
- , _faction :: Faction
+   _faction :: Faction
  , _pos :: Coord
  , _hp :: Int
  , _ap :: Int
+ , _id :: Int
 }
-  deriving (Show)
+  deriving (Show, Eq)
 
 data GameState = GameState {
   _map :: Set Coord
 , _walls :: Set Coord
-, _units :: Set Unit
-, _next :: [Unit]
-, _resolved :: [Unit]
+, _units :: Map Int Unit
+, _next :: [Int]
+, _resolved :: [Int]
 , _rounds :: Int
+, _complete :: Bool
 }
   deriving (Show)
 
@@ -39,11 +40,7 @@ instance Ord Coord
   where compare (Coord (x0, y0)) (Coord (x1, y1)) = compare (y0, x0) (y1, x1)
 
 instance Ord Unit
-  where compare u u'
-          | _id u == _id u' = EQ
-          | otherwise = compare (_pos u) (_pos u')
-instance Eq Unit
-  where (==) u u' = _id u == _id u'
+  where compare u u' = compare (_pos u) (_pos u')
 
 compLength :: [a] -> [b] -> Ordering
 compLength a b = compare (length a) (length b)
@@ -52,25 +49,27 @@ compSnd :: Ord b => (a, b) -> (a, b) -> Ordering
 compSnd a b = compare (snd a) (snd b)
 
 compUnitHp :: Unit -> Unit -> Ordering
-compUnitHp a b = compare (_hp a, -y0, x0) (_hp b, -y1, x1)
+compUnitHp a b = compare (_hp a, y0, x0) (_hp b, y1, x1)
   where (Coord (x0, y0)) = _pos a
         (Coord (x1, y1)) = _pos b
 
 -- Input parsing and printing
-toUnit :: Char -> Coord -> Unit
-toUnit 'E' p@(Coord (x,y)) = Unit (read $ (show x) ++ (show y)) Elf p 200 3
-toUnit 'G' p@(Coord (x,y)) = Unit (read $ (show x) ++ (show y)) Goblin p 200 3
+toUnit :: Char -> Coord -> (Int, Unit)
+toUnit 'E' p@(Coord (x,y)) = ((uid x y), Unit Elf p 200 3 (uid x y))
+  where uid x y = (read $ (show x) ++ (show y))
+toUnit 'G' p@(Coord (x,y)) = ((uid x y), Unit Goblin p 200 3 (uid x y))
+  where uid x y = (read $ (show x) ++ (show y))
 toUnit _ _ = error "Not a valid unit"
 
 strToBoard :: (Int, String) -> GameState -> GameState
 strToBoard (y, str) gs  = gs { _map = (S.union (_map gs) coords)
                        , _walls = (S.union (_walls gs) walls)
-                       , _units = (S.union (_units gs) units)
+                       , _units = (M.union (_units gs) units)
                        }
   where inpt = zip str [Coord (i, y) | i <- [1..]]
         coords = S.fromList $ map snd inpt
         walls = S.fromList $ map snd . filter ((==) '#' . fst) $ inpt
-        units = S.fromList $ map (uncurry toUnit) . filter ((`elem` "EG") . fst) $ inpt
+        units = M.fromList $ map (uncurry toUnit) . filter ((`elem` "EG") . fst) $ inpt
 
 linesToBoard :: GameState -> [String] -> GameState
 linesToBoard init s = foldr strToBoard init (zip [1..] s)
@@ -80,9 +79,9 @@ printBoard gameState = unlines .
   chunksOf maxX .
   map fst . sortBy compSnd $ allSpaces
   where allSpaces = S.toList $ S.unions [allUnits, allWalls, emptySpace]
-        allUnits = S.map (\x -> (if (_faction x) == Elf then 'E' else 'G', _pos x)) (_units gameState)
+        allUnits = S.map (\x -> (if (_faction x) == Elf then 'E' else 'G', _pos x)) (S.fromList $ map snd (M.toList $ _units gameState))
         allWalls = S.map (\x -> ('#', x)) (_walls gameState)
-        emptySpace = S.map (\x -> ('.', x)) (_map gameState S.\\ (S.union (_walls gameState) (S.map _pos (_units gameState))))
+        emptySpace = S.map (\x -> ('.', x)) (_map gameState S.\\ (S.union (_walls gameState) (S.fromList $ (map (_pos . snd) (M.toList $ _units gameState)))))
         maxX = maximum $ map ((\(Coord (x, y)) -> x) . snd) allSpaces
 
 -- Pathing and targeting
@@ -93,39 +92,42 @@ closestTarget :: Set Coord -> Set Coord -> Coord -> Maybe Coord
 closestTarget universe candidates origin = go universe candidates (S.singleton origin)
   where go valid targets exhausted
           | (not . S.null) $ S.intersection candidates exhausted = S.lookupMin (S.intersection candidates exhausted)
-          | S.null (withAdjacencies exhausted S.\\ exhausted) = Nothing
-          | otherwise = go universe targets (withAdjacencies exhausted)
+          | withAdjacencies exhausted == S.empty = Nothing
+          | otherwise = go universe targets (S.union exhausted (withAdjacencies exhausted))
         sOrigin = S.singleton origin
-        validAdj visited point = S.intersection universe (adjacent point) S.\\ visited
+        validAdj visited point = (S.intersection universe (adjacent point)) S.\\ visited
         withAdjacencies visited = S.unions (map (validAdj visited) (S.toList visited))
 
 moveUnit :: Unit -> Coord -> Unit
 moveUnit u newPos = u { _pos = newPos }
 
 passable :: GameState -> Faction -> Set Coord
-passable gs fac = (_map gs) S.\\
-  (S.union (_walls gs) (S.map _pos (S.filter ((== fac) . _faction) (_units gs))))
+passable gs fac = _map gs S.\\
+  S.union (_walls gs) (S.map _pos (S.filter ((== fac) . _faction) (S.fromList (map snd (M.toList $ _units gs)))))
+
+allPassable :: GameState -> Set Coord
+allPassable gs = _map gs S.\\ S.union (_walls gs) (S.map _pos (S.fromList (map snd (M.toList $ _units gs))))
 
 enemyInRange :: GameState -> Unit -> Maybe Unit
 enemyInRange gs u = case S.toList $ S.filter ((`elem` adjacencies) . _pos) enemies of
   [] -> Nothing
-  x -> Just $ head . sortBy compUnitHp $ x
+  x -> Just $ minimumBy compUnitHp $ x
   where adjacencies = S.intersection (adjacent (_pos u)) (passable gs (_faction u))
-        enemies = S.filter ((/= _faction u) . _faction) $ (_units gs)
+        enemies = S.filter ((/= _faction u) . _faction) $ S.fromList (map snd (M.toList $ _units gs))
 
 findTarget :: GameState -> Unit -> Set Unit -> Maybe Coord
-findTarget gs u enemies = closestTarget (passable gs (_faction u)) (S.map _pos enemies) (_pos u)
+findTarget gs u enemies = closestTarget (allPassable gs) adjEnemySquares (_pos u)
+  where adjEnemySquares = S.intersection (S.unions $ map (adjacent . _pos) (S.toList enemies)) (allPassable gs)
 
 -- Actions
-
 attackUnit :: Unit -> Unit -> [Unit]
 attackUnit unit target = [unit, target { _hp = _hp target - _ap unit }]
 
 enemies :: GameState -> Unit -> Set Unit
-enemies gs u = S.filter ((/= _faction u) . _faction) (_units gs)
+enemies gs u = S.filter ((/= _faction u) . _faction) (S.fromList $ map snd (M.toList $ _units gs))
 
 nextMove :: GameState -> Unit -> Maybe Coord
-nextMove gs u = case (findTarget gs u (enemies gs u)) of
+nextMove gs u = case findTarget gs u (enemies gs u) of
   Nothing -> Nothing
   (Just targetPos) -> closestTarget (passable gs (_faction u)) validAdj targetPos
   where valid = passable gs (_faction u)
@@ -133,25 +135,20 @@ nextMove gs u = case (findTarget gs u (enemies gs u)) of
 
 cleanupDead :: GameState -> GameState
 cleanupDead gs = gs { _units = alive }
-  where alive = S.filter ((> 0) . _hp) (_units gs)
+  where alive = M.filter ((> 0) . _hp) (_units gs)
 
 updateUnits :: GameState -> [Unit] -> GameState
-updateUnits gs@(GameState { _units = u }) updated = gs { _units = S.union newUnits u }
-  where newUnits = (S.fromList updated)
+updateUnits gs@(GameState { _units = u }) updated = gs { _units = M.union newUnits u }
+  where newUnits = M.fromList $ map (\x -> (_id x, x)) updated
 
 -- Gameplay
 nextTurn :: GameState -> GameState
-nextTurn gs@(GameState {
-   _next = x:[]
- , _resolved = ys
- , _rounds = i
- }) = gs { _next = sort ys, _resolved = [], _rounds = i + 1 }
-nextTurn gs@(GameState { _next = x:xs, _resolved = ys }) = gs { _next = xs, _resolved = x:ys }
-nextTurn gs@(GameState { _next = [], _resolved = [] }) = gs { _next = sort (S.toList $ _units gs) }
+nextTurn gs@GameState { _next = x:xs, _resolved = ys } = gs { _next = xs, _resolved = x:ys }
+nextTurn gs@GameState { _next = [], _resolved = [] } = gs { _next = (map fst) $ sortBy compSnd (M.toList $ _units gs) }
 
 resolveAttacks :: Unit -> GameState -> GameState
 resolveAttacks u gs = case enemyInRange gs u of
-  (Just enemy) -> nextTurn . cleanupDead . updateUnits gs $ attackUnit u enemy
+  (Just enemy) -> cleanupDead . updateUnits gs $ attackUnit u enemy
   Nothing -> gs
 
 resolveMoves :: Unit -> GameState -> (Unit, GameState)
@@ -162,13 +159,36 @@ resolveMoves u gs = case enemyInRange gs u of
     Nothing -> (u, gs)
 
 resolveUnit :: Unit -> GameState -> GameState
-resolveUnit u gs = uncurry resolveAttacks . resolveMoves u $ gs
+resolveUnit u = uncurry resolveAttacks . resolveMoves u
 
-testGrid = S.fromList $ [ (i, j) | i <- [1..100]::[Int], j <- [1..100]::[Int] ]
-emptyState = GameState S.empty S.empty S.empty [] [] 0
+testGrid = S.fromList [ (i, j) | i <- [1..100]::[Int], j <- [1..100]::[Int] ]
+emptyState = GameState S.empty S.empty M.empty [] [] 0 False
+
+nextRound gs@GameState {
+   _resolved = ys
+ , _rounds = i
+ , _units = u
+ , _complete = False
+ } = gs { _next = (map fst) . (sortBy compSnd) $ (M.toList u), _resolved = [], _rounds = i + 1 }
+nextRound gs@GameState { _complete = True } = gs
+
+getUnit :: Int -> GameState -> Maybe Unit
+getUnit id gs = M.lookup id (_units gs)
 
 resolveRound :: GameState -> GameState
-resolveRound gs@(GameState { _next = [] }) = gs
-resolveRound gs@(GameState { _next = x:xs }) = resolveRound (resolveUnit x gs)
+resolveRound gs@GameState { _next = [] } = gs
+resolveRound gs@GameState { _next = x:xs } = case getUnit x gs of
+  Nothing -> resolveRound . nextTurn $ gs
+  (Just u) -> case gameOver u gs of
+    True -> gs { _complete = True }
+    False -> resolveRound . nextTurn . resolveUnit u $ gs
+
+gameOver :: Unit -> GameState -> Bool
+gameOver u gs = case length $ S.toList (enemies gs u) of
+    0 -> True
+    _ -> False
+
+summarizeGame :: GameState -> (Int, Int)
+summarizeGame GameState { _units = units, _rounds = r } = (sum (map (_hp . snd) $ M.toList units), r)
 
 txt = readFile "./input/day15.txt"
